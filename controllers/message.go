ccdf8fc6ad6565737df171f9e369dca180ea8367
Louise van der Lith <louisevanderlith@gmail.com>
2019-09-08 21:46:21 +0200

    Updated modules

--- controllers/message.go
+++ controllers/message.go
@@ -8,7 +8,13 @@ import (
 	"github.com/louisevanderlith/husk"
 )
 
-type MessageController struct {
+type Messages struct {
+}
+
+func (x *Messages) Get(ctx context.Requester) (int, interface{}) {
+	result := core.GetMessages(1, 10)
+
+	return http.StatusOK, result
 }
 
 // @Title SendMessage
@@ -17,7 +23,7 @@ type MessageController struct {
 // @Success 200 {map[string]string} map[string]string
 // @Failure 403 body is empty
 // @router / [post]
-func (req *MessageController) Post(ctx context.Contexer) (int, interface{}) {
+func (req *Messages) Create(ctx context.Requester) (int, interface{}) {
 	var message core.Message
 	err := ctx.Body(&message)
 
@@ -41,7 +47,7 @@ func (req *MessageController) Post(ctx context.Contexer) (int, interface{}) {
 // @Description Gets all Messages
 // @Success 200 {[]comms.Message]} []comms.Message]
 // @router /all/:pagesize [get]
-func (req *MessageController) Get(ctx context.Contexer) (int, interface{}) {
+func (req *Messages) Search(ctx context.Requester) (int, interface{}) {
 	page, size := ctx.GetPageData()
 	result := core.GetMessages(page, size)
 
@@ -53,7 +59,7 @@ func (req *MessageController) Get(ctx context.Contexer) (int, interface{}) {
 // @Param	key			path	string 	true		"comms key"
 // @Success 200 {core.Message} core.Message
 // @router /:key [get]
-func (req *MessageController) GetOne(ctx context.Contexer) (int, interface{}) {
+func (req *Messages) View(ctx context.Requester) (int, interface{}) {
 	siteParam := ctx.FindParam("key")
 
 	key, err := husk.ParseKey(siteParam)
--- routers/router.go
+++ routers/router.go
@@ -6,43 +6,15 @@ import (
 	"github.com/louisevanderlith/droxolite/mix"
 	"github.com/louisevanderlith/droxolite/resins"
 	"github.com/louisevanderlith/droxolite/roletype"
-	"github.com/louisevanderlith/droxolite/routing"
 )
 
-func Setup(poxy resins.Epoxi) {
+func Setup(e resins.Epoxi) {
+	e.JoinBundle("/", roletype.User, mix.JSON, &controllers.Messages{})
 	//Message
-	msgCtrl := &controllers.MessageController{}
+	/*msgCtrl := &controllers.MessageController{}
 	msgGroup := routing.NewRouteGroup("message", mix.JSON)
 	msgGroup.AddRoute("Create Message", "", "POST", roletype.Unknown, msgCtrl.Post)
 	msgGroup.AddRoute("All Messages", "/all/{pagesize:[A-Z][0-9]+}", "GET", roletype.Admin, msgCtrl.Get)
 	msgGroup.AddRoute("View Message", "/{key:[0-9]+\x60[0-9]+}", "GET", roletype.Unknown, msgCtrl.GetOne)
-	poxy.AddGroup(msgGroup)
-	/*ctrlmap := EnableFilters(s, host)
-
-	msgCtrl := controllers.NewMessageCtrl(ctrlmap)
-
-	beego.Router("/v1/message", msgCtrl, "post:Post")
-	beego.Router("/v1/message/:key", msgCtrl, "get:GetOne")
-	beego.Router("/v1/message/all/:pageSize", msgCtrl, "get:Get")*/
+	e.AddBundle(msgGroup)*/
 }
-
-/*
-func EnableFilters(s *mango.Service, host string) *control.ControllerMap {
-	ctrlmap := control.CreateControlMap(s)
-
-	emptyMap := make(secure.ActionMap)
-	emptyMap["GET"] = roletype.Admin
-
-	ctrlmap.Add("/v1/message", emptyMap)
-
-	beego.InsertFilter("/v1/*", beego.BeforeRouter, ctrlmap.FilterAPI, false)
-	allowed := fmt.Sprintf("https://*%s", strings.TrimSuffix(host, "/"))
-
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins: []string{allowed},
-		AllowMethods: []string{"GET", "POST", "OPTIONS"},
-	}), false)
-
-	return ctrlmap
-}
-*/
