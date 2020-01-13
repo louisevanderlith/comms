68345cbcd69f8c092048c08b7c1ac2dedff008bc
Vicky <vickydejager19@gmail.com>
2020-01-13 21:29:40 +0200

    updated controllers and main file

--- main.go
+++ main.go
@@ -16,38 +16,63 @@ import (
 )
 
 func main() {
-	keyPath := os.Getenv("KEYPATH")
-	pubName := os.Getenv("PUBLICKEY")
-	host := os.Getenv("HOST")
-	httpport, _ := strconv.Atoi(os.Getenv("HTTPPORT"))
-	appName := os.Getenv("APPNAME")
-	pubPath := path.Join(keyPath, pubName)
-
-	// Register with router
-	srv := bodies.NewService(appName, "", pubPath, host, httpport, servicetype.API)
-
-	routr, err := do.GetServiceURL("", "Router.API", false)
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = srv.Register(routr)
-
-	if err != nil {
-		panic(err)
-	}
-
-	poxy := resins.NewMonoEpoxy(srv, element.GetNoTheme(host, srv.ID, "none"))
-	routers.Setup(poxy)
-	poxy.EnableCORS(host)
-
 	core.CreateContext()
 	defer core.Shutdown()
 
-	err = droxolite.Boot(poxy)
+	r := gin.Default()
+
+	r.GET("/article/:key", article.View)
+
+	articles := r.Group("/article")
+	articles.POST("", article.Create)
+	articles.PUT("/:key", article.Update)
+	articles.DELETE("/:key", article.Delete)
+
+	r.GET("/articles", article.Get)
+	r.GET("/articles/:pagesize/*hash", article.Search)
+
+	r.POST("/submit/Submit Contact", msgCtrl.Create)
+
+	err := r.Run(":8085")
 
 	if err != nil {
 		panic(err)
 	}
 }
+
+// func main() {
+// 	keyPath := os.Getenv("KEYPATH")
+// 	pubName := os.Getenv("PUBLICKEY")
+// 	host := os.Getenv("HOST")
+// 	httpport, _ := strconv.Atoi(os.Getenv("HTTPPORT"))
+// 	appName := os.Getenv("APPNAME")
+// 	pubPath := path.Join(keyPath, pubName)
+
+// 	// Register with router
+// 	srv := bodies.NewService(appName, "", pubPath, host, httpport, servicetype.API)
+
+// 	routr, err := do.GetServiceURL("", "Router.API", false)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	err = srv.Register(routr)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	poxy := resins.NewMonoEpoxy(srv, element.GetNoTheme(host, srv.ID, "none"))
+// 	routers.Setup(poxy)
+// 	poxy.EnableCORS(host)
+
+// 	core.CreateContext()
+// 	defer core.Shutdown()
+
+// 	err = droxolite.Boot(poxy)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
