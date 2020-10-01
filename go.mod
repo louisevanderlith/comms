3406ec41c80c50d9cf0f77ef4fe62ddaa2ab7502
louisevanderlith <louisevanderlith@gmail.com>
2020-10-01 13:41:17 +0200

    Fixing Consent

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/louisevanderlith/comms
 
 require (
 	github.com/gorilla/mux v1.7.4
-	github.com/louisevanderlith/droxolite v1.11.8
-	github.com/louisevanderlith/husk v1.7.1
-	github.com/louisevanderlith/kong v1.6.6
+	github.com/louisevanderlith/droxolite v1.12.7
+	github.com/louisevanderlith/husk v1.7.4
+	github.com/louisevanderlith/kong v1.7.3
 	github.com/rs/cors v1.6.0
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
--- handles/handler.go
+++ handles/handler.go
@@ -2,14 +2,14 @@ package handles
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/louisevanderlith/kong"
+	"github.com/louisevanderlith/kong/middle"
 	"github.com/rs/cors"
 	"net/http"
 )
 
 func SetupRoutes(securityUrl, managerUrl, scrt, smtpUser, smtpPass, smtpHost string, smtpPort int) http.Handler {
 	r := mux.NewRouter()
-	ins := kong.NewResourceInspector(http.DefaultClient, securityUrl, managerUrl)
+	ins := middle.NewResourceInspector(http.DefaultClient, securityUrl, managerUrl)
 	view := ins.Middleware("comms.messages.view", scrt, ViewMessage)
 	r.HandleFunc("/message/{key:[0-9]+\\x60[0-9]+}", view).Methods(http.MethodGet)
 
@@ -20,7 +20,7 @@ func SetupRoutes(securityUrl, managerUrl, scrt, smtpUser, smtpPass, smtpHost str
 	r.HandleFunc("/message/{pagesize:[A-Z][0-9]+}", search).Methods(http.MethodGet)
 	r.HandleFunc("/message/{pagesize:[A-Z][0-9]+}/{hash:[a-zA-Z0-9]+={0,2}}", search).Methods(http.MethodGet)
 
-	lst, err := kong.Whitelist(http.DefaultClient, securityUrl, "comms.messages.view", scrt)
+	lst, err := middle.Whitelist(http.DefaultClient, securityUrl, "comms.messages.view", scrt)
 
 	if err != nil {
 		panic(err)
